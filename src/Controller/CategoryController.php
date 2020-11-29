a85cb986707ca2004da34172012a925f5e5763d8
ClaireV38 <claireverdier00@gmail.com>
2020-11-29 17:14:23 +0100

    use method findby() from ProgramRepository's parent class ti dipslay the three last programs from one category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 // src/Controller/CategoryController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +27,31 @@ class CategoryController extends AbstractController
 
         return $this->render('category/index.html.twig', [
             'categories' => $categories
+        ]);
+    }
+
+    /**
+     *Show up to 3 series from one category
+     *
+     * @Route("/{categoryName}", requirements={"category"="\w+"}, methods={"GET"}, name="show")
+     * @param string $categoryName
+     * @return Response
+     */
+    public function show(string $categoryName): Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $categoryName]);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with name : ' . $categoryName . ' found in program\'s table.'
+            );
+        }
+        $programsByCategory = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category' => $category], ['id' => 'DESC'], 3);
+        return $this->render('category/show.html.twig', [
+            'programs' => $programsByCategory,
         ]);
     }
 }
