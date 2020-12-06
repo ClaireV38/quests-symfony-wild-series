8db1e3e21aca9b281cdb463bff877a0d643dcf06
ClaireV38 <claireverdier00@gmail.com>
2020-12-06 19:44:51 +0100

    start creating method show in ActorController

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use http\Env\Response;
+use App\Entity\Actor;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ActorController
+ * @Route("/actors", name="actor_")
+ */
+class ActorController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @route("/{id}", name="show")
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+
+    }
+}
