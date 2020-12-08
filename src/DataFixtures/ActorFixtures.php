2281e9b68a5354d5371424fc84cb28bb4f8489dd
ClaireV38 <claireverdier00@gmail.com>
2020-12-08 21:38:13 +0100

    create class actor fixture

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Persistence\ObjectManager;
+
+class ActorFixtures extends \Doctrine\Bundle\FixturesBundle\Fixture
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
