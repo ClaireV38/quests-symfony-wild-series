47b3dcc956a662eb084be896cf59a5206f0862fa
ClaireV38 <claireverdier00@gmail.com>
2021-01-02 18:16:56 +0100

    traduct home page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
