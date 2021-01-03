993703ede039ab2e20277ec5964715bfa10734fb
ClaireV38 <claireverdier00@gmail.com>
2021-01-03 12:24:14 +0100

    modify create episode form

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -4,11 +4,13 @@ namespace App\Form;
 
 use App\Form\CommentType;
 use App\Entity\Episode;
+use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class EpisodeType extends AbstractType
 {
@@ -18,11 +20,11 @@ class EpisodeType extends AbstractType
             ->add('title')
             ->add('number')
             ->add('synopsis')
-            ->add('season')
-            ->add('comments', CollectionType::class, [
-                'entry_type' => CommentType::class,
-                'entry_options' => ['label' => false],
-            ]);
+            ->add('season', EntityType::class, ['class' => Season::class,
+               'choice_label' => function ($season, $key, $value) {
+                    return ($season->getNumber() ." ". $season->getProgram()->getTitle());
+               }
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
