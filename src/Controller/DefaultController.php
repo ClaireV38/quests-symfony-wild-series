1c9a04079465b1a7ff1cad2c3240c87ad2b0a57f
ClaireV38 <claireverdier00@gmail.com>
2021-01-11 10:51:52 +0100

    start adding dropdown menu with categories

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CategoryRepository;
 
 class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
@@ -22,6 +23,13 @@ class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\Abstr
         return $this->render('index.html.twig', [
             'website' => 'Wild Series',
             'programs' => $programs,
+        ]);
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
         ]);
     }
 }
